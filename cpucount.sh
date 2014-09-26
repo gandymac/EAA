073497176557d7764b5f1a6a4b7bf882d3737a52
gandymac <gandymac@outlook.com>
2014-09-26 15:47:45 +0100

    The Modified version of the cpucount.sh file

--- cpucount.sh
+++ cpucount.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+#: Title : cpucount.sh
+#: Date : 26/9/2014
+#: Author : Andrew McGuinness
+#: Version : 1.0
+#: Description : CPU count Script 
+#: Options : 
+
 grep processor /proc/cpuinfo | wc -l
